97cc3ec1300a57a04aaf8bfd94d771ce7b0abe11
1of12apoos <darr3n56@gmail.com>
2026-09-13 03:27:39 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sunrise-v2';
+const CACHE = 'sunrise-v3';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
